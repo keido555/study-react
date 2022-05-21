15333328837ca1bdbab66759cbe34a22709a7775
keido <w03optfd73@gmail.com>
2022-05-21 11:57:41 +0900

    ♻️ Main部分をコンポーネント化

--- components/Headline.jsx
+++ components/Headline.jsx
@@ -1,14 +1,14 @@
 import styles from "../styles/Home.module.css";
 
 export function Headline(props) {
-  const { title, page } = props;
+  const { page, onClick, children } = props;
   return (
     <div>
-      <h1 className={styles.title}>{title}</h1>
+      <h1 className={styles.title}>{page} page</h1>
 
-      <p className={styles.description}>
-        Get started by editing <code className={styles.code}>pages/{page}</code>
-      </p>
+      <p className={styles.description}>Get started by editing {children}</p>
+
+      <button onClick={onClick}>ボタン</button>
     </div>
   );
 }
