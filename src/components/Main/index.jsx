ea24d7050904a570703f816f1583b4d658d3f249
keido <w03optfd73@gmail.com>
2022-05-22 21:49:51 +0900

    ✨カウンターを設置してuseStateでカウントアップ

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,8 @@
-import { useEffect } from "react";
 import classes from "src/components/Main/Main.module.css";
 import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
 
 export function Main(props) {
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
-
   return (
     <main className={classes.main}>
       <Headline page={props.page}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
@@ -6,11 +6,19 @@ import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
+  const [count, setCount] = useState(0);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
@@ -19,9 +27,8 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <button href="/about" onClick={handleClick}>
-        ボタン
-      </button>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
 
       <Footer />
