58f8c9ce3923faa808628ef7b360a63b62d3ef4e
keido <w03optfd73@gmail.com>
2022-05-23 10:59:44 +0900

    🎨 Custom Hooksを使ってまとめる

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,53 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内の入力");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => {
-      return !prevIsShow;
-    });
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -55,10 +19,13 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>追加</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <br />
+      <br />
+
       <input type="text" value={text} onChange={handleChange} />
       <br />
       <button onClick={handleAdd}>追加</button>
@@ -67,6 +34,7 @@ export default function Home() {
           return <li key={item}>{item}</li>;
         })}
       </ul>
+
       <Main page="index" />
 
       <Footer />
