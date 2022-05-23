ac4e7b912ac6a55285a3bf1445c82bd0f035473b
keido <w03optfd73@gmail.com>
2022-05-23 10:10:18 +0900

    ✨ useStateの配列

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -30,6 +31,16 @@ export default function Home() {
     });
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
 
@@ -49,6 +60,13 @@ export default function Home() {
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <br />
       <input type="text" value={text} onChange={handleChange} />
+      <br />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
 
       <Footer />
