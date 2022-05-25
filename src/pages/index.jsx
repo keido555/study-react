45c47dc7994dd1fad9e3c3c585e9a1bb44128271
keido <w03optfd73@gmail.com>
2022-05-25 21:06:09 +0900

    🚸 APIを叩く際のローディング状態などを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
+import { Posts } from "src/components/posts/index";
 
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,13 +11,7 @@ const Home = (props) => {
       </Head>
       <Header />
 
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
