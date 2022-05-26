2bf23d4c72ea866fae1f547dbd1f0941d908edd9
keido <w03optfd73@gmail.com>
2022-05-26 23:04:37 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -2,7 +2,7 @@ import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Post } from "src/components/post";
 
-const PostId = () => {
+const PostsId = () => {
   return (
     <div className={styles.container}>
       <Header />
@@ -12,4 +12,4 @@ const PostId = () => {
   );
 };
 
-export default PostId;
+export default PostsId;
