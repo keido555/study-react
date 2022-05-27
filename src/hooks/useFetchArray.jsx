e08a8865a1c5f9d3dbe5974863b2d28f4f90627a
keido <w03optfd73@gmail.com>
2022-05-27 16:30:12 +0900

    ♻️ 配列のデータのAPIを叩くときの処理を共通化

---
+++ src/hooks/useFetchArray.jsx
@@ -0,0 +1,28 @@
+import { fetcher } from "src/utils/fetcher";
+import useSWR from "swr";
+
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const useFetchArray = (url) => {
+  const { data, error } = useSWR(url, fetcher);
+  return {
+    data,
+    error,
+    isLoading: !error && !data,
+    isEmpty: data && data.length === 0,
+  };
+};
+
+const API_URL = "https://jsonplaceholder.typicode.com";
+
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`);
+};
+
+export const usePosts = () => {
+  return useFetchArray(`${API_URL}/posts`);
+};
+
+export const useUsers = () => {
+  return useFetchArray(`${API_URL}/users`);
+};
