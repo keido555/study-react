d41f67b35730ffa07f02c5180d227f687a220347
keido <w03optfd73@gmail.com>
2022-05-28 22:40:49 +0900

    ➕ tailwind css のセットアップ

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
