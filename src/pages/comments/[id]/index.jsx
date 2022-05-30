cfe14ee749269459d41f03d2c9d2d79bed14cd13
keido <w03optfd73@gmail.com>
2022-05-30 16:49:52 +0900

    ISRを試すために導入

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -24,6 +24,7 @@ export const getStaticProps = async (ctx) => {
   if (!comment.ok) {
     return {
       notFound: true,
+      revalidate: 1,
     };
   }
 
@@ -35,6 +36,7 @@ export const getStaticProps = async (ctx) => {
         [COMMENT_API_URL]: commentData,
       },
     },
+    revalidate: 1,
   };
 };
 
