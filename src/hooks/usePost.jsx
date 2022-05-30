ddb659831527d029c900a2e87996dd926437c8ca
keido <w03optfd73@gmail.com>
2022-05-30 15:52:13 +0900

    🎨 APIのURLをconstから読み込むように変更

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -1,11 +1,8 @@
-import { fetcher } from "src/utils/fetcher";
+import { API_URL } from "src/utils/const";
 import useSWR from "swr";
 
 export const usePost = (id) => {
-  const { data, error } = useSWR(
-    id ? `https://jsonplaceholder.typicode.com/posts/${id}` : null
-    // fetcher
-  );
+  const { data, error } = useSWR(id ? `${API_URL}/posts/${id}` : null);
 
   return {
     data,
--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import { CommentsComponent } from "src/components/Comments";
 import { Header } from "src/components/Header";
+import { API_URL } from "src/utils/const";
 import { SWRConfig } from "swr";
 
 // サーバーで実行されているコード
 export const getStaticProps = async () => {
-  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  const COMMENTS_API_URL = `${API_URL}/comments`;
   const comments = await fetch(COMMENTS_API_URL);
   const commentsData = await comments.json();
 
